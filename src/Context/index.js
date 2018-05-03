b25146650aa68ada802eea58fa9bc70d6abab827
jingduo <jingduo420@126.com>
2018-05-03 14:19:02 +0800

    update

--- src/Context/index.js
+++ src/Context/index.js
@@ -1,16 +1,16 @@
-import React,{ Component } from 'react'
+import React, {Component} from 'react'
 import CounterContainer from './CounterContainer'
 import SummaryContainer from './SummaryContainer'
 
 class ControlPanel extends Component {
-  render () {
+  render() {
     return (
       <div>
         <h5>context</h5>
-        <CounterContainer caption='First' />
-        <CounterContainer caption='Second' />
-        <CounterContainer caption='Third' />
-        <SummaryContainer />
+        <CounterContainer caption='First'/>
+        <CounterContainer caption='Second'/>
+        <CounterContainer caption='Third'/>
+        <SummaryContainer/>
       </div>
     )
   }
