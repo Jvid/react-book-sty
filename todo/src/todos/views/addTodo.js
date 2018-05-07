ac91cd8da0295c3e9f007a0fafe76d2b2b8998c2
jingduo <jingduo420@126.com>
2018-05-07 15:35:29 +0800

    remove ref

--- todo/src/todos/views/addTodo.js
+++ todo/src/todos/views/addTodo.js
@@ -8,28 +8,33 @@ class AddTodo extends Component {
     super(props,context)
     // this.input = ''
     this.onSubmit = this.onSubmit.bind(this)
-    this.refInput = this.refInput.bind(this)
+    this.onInputChange = this.onInputChange.bind(this)
+    this.state = {
+      value: ''
+    }
   }
 
   onSubmit(ev) {
     ev.preventDefault()
-    const input = this.input
-    if(!input.value.trim()) {
+    const inputValue = this.state.value
+    if (!inputValue.trim()) {
       return
     }
-    this.props.onAdd(input.value)
-    input.value = ''
+    this.props.onAdd(inputValue)
+    this.setState({value: ''})
   }
 
-  refInput(node) {
-    this.input = node
+  onInputChange(event) {
+    this.setState({
+      value: event.target.value
+    })
   }
 
   render() {
     return (
       <div className="add-todo">
         <form onSubmit={this.onSubmit}>
-          <input type="text" className="new-todo" ref={this.refInput} />
+          <input type="text" className="new-todo" onChange={this.onInputChange} value={this.state.value} />
           <button className="add-btn" type="submit">添加</button>
         </form>
       </div>
